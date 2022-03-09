c56ca70a76e5aa29fc0e1061fe2c695f1412eef0
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-09 17:21:45 +0000

    creating a theme for whatsapp clone

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WhatsAppTheme {
-
+  static const darkThemeColor = Color(0xFF);
+  static const lightThemeColor = Color(0xFF);
+  TextStyle textHeading1 = GoogleFonts.poppins();
 }
