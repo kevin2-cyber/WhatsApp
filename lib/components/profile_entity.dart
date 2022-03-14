10d43bb498c7369a4086e4da6c37bd0e0d602e0c
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-14 19:58:51 +0000

    Entity class

--- lib/components/profile_entity.dart
+++ lib/components/profile_entity.dart
@@ -2,6 +2,7 @@ import 'package:whatsapp_clone/components/chat_items.dart';
 
 class Profiles {
   List<ChatEntity> profiles = <ChatEntity>[
-
+    ChatEntity(imageUrl: 'assets/images/angela.jpeg', userName: 'Angela Yu'),
+    ChatEntity(imageUrl: 'assets/images/cpp.jpeg', userName: 'Creator of CPP'),
   ];
 }
