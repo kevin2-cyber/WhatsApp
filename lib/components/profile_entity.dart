0d75d9809bbe47e0cca140dabe7de5ec94b1a83a
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-15 09:21:58 +0000

    to be fixed on branch

--- lib/components/profile_entity.dart
+++ lib/components/profile_entity.dart
@@ -1,7 +1,7 @@
 import 'package:whatsapp_clone/components/chat_items.dart';
 
 class Profiles {
-  List<ChatEntity> profiles = <ChatEntity>[
+  List<ChatEntity> profiles = [
     ChatEntity(imageUrl: 'assets/images/angela.jpeg', userName: 'Angela Yu'),
     ChatEntity(imageUrl: 'assets/images/cpp.jpeg', userName: 'Creator of CPP'),
   ];
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/components/profile_entity.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    Profiles? profiles;
     return Container(
       child: ListView.builder(
-          itemBuilder: ),
+        itemCount: profiles?.profiles.length,
+          itemBuilder: (context, index) {
+          return profiles
+          }
+      )),;
     );
   }
 }
