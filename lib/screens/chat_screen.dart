ed5711675b808728bf92525420342a3ee01fc2fb
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-26 14:03:58 +0000

    refactoring

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone/components/profile_entity.dart';
+// import 'package:whatsapp_clone/components/profile_entity.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    // Profiles? profiles;
     return Container(
-
+      child: ListView.builder(itemBuilder: ((context, index) => )),
     );
   }
 }
