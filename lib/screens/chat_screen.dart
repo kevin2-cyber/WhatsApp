1296fe6fa0dbeadc33fe9ff4a78960954e8fca1d
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-27 01:17:52 +0000

    commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
 
-
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(itemBuilder: ((context, index) => )),
-    );
     return Container();
-
   }
 }
-
-
-// child: ListView.builder(
-// itemCount: profiles?.profiles.length,
-// itemBuilder: (context, index) {
-// return profiles;
-// }
-// )
