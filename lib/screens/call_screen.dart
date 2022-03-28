9a44dd6739759d70bdb9a41c989196ab54f5a02c
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-28 21:41:55 +0000

    built call screen

--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -6,6 +6,20 @@ class CallScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        children: [
+          const Text('To start calling contacts who have'),
+          Row(
+            children: const [
+              Text('WhatsApp, tap'),
+              Icon(Icons.add_call),
+              Text('at the bottom  of your')
+            ],
+          ),
+          const Text('screen.')
+        ],
+      ),
+    );
   }
 }
