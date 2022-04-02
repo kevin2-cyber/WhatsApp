87d16c6ac2a54c1380b8f1829fbec66b69d26ab6
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-02 15:12:01 +0000

    push

--- lib/screens/status_screen.dart
+++ lib/screens/status_screen.dart
@@ -5,28 +5,6 @@ class StatusScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // child: Stack(
-        //   children: [
-        //     Positioned(
-        //       top: 620,
-        //       left: 350,
-        //       child: SizedBox(
-        //         height: 40,
-        //         width: 40,
-        //         child: FloatingActionButton(
-        //           backgroundColor: WhatsAppTheme.kdarkAppBarColor,
-        //           onPressed: () {},
-        //           child: const Icon(
-        //             Icons.edit,
-        //             color: Colors.white,
-        //             size: 20.0,
-        //           ),
-        //         ),
-        //       ),
-        //     ),
-        //   ],
-        // ),
-        );
+    return Container();
   }
 }
