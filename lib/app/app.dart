2bd3b1d73a1d85d3e287ca2b9c6bdd20d14b3cc3
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-17 01:29:08 +0000

    almost done setting up

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WhatsApp extends StatelessWidget {
+  const WhatsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
