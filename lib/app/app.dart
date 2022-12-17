e7d7420206145b7bb27448e54f82ddd906e505a6
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-17 09:43:06 +0000

    making some adjustments

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WhatsApp extends StatelessWidget {
   const WhatsApp({Key? key}) : super(key: key);
@@ -7,6 +8,15 @@ class WhatsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData.light().copyWith(
+        textTheme: GoogleFonts.lexendTextTheme(),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        textTheme: GoogleFonts.lexendTextTheme(),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: Scaffold(
         body: Container(),
       ),
