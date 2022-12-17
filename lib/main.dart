6a12e3686023198b64789f0ddb3c2dc51bcbb063
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-17 09:38:51 +0000

    added an error widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:whatsapp_clone/app/app.dart';
 
 void main() {
 
+  ErrorWidget.builder = (FlutterErrorDetails details) => Material(
+    color: Colors.green.shade900.withBlue(20),
+    child: Center(
+      child: Text(
+        details.exceptionAsString(),
+        style: GoogleFonts.lexendDeca(
+          color: Colors.grey,
+          fontWeight: FontWeight.w300,
+          fontSize: 18,
+        ),
+      ),
+    ),
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const WhatsApp());
 }
