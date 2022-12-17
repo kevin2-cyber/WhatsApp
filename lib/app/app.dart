577a0e3db3d54d66bce92c90b3499bd30675ab82
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-17 10:25:30 +0000

    using the original font for whatsapp

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:whatsapp_clone/app/core/constants.dart';
 
 class WhatsApp extends StatelessWidget {
@@ -12,11 +11,9 @@ class WhatsApp extends StatelessWidget {
       title: AppConstants.title,
       themeMode: ThemeMode.system,
       theme: ThemeData.light().copyWith(
-        textTheme: GoogleFonts.lexendTextTheme(),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       darkTheme: ThemeData.dark().copyWith(
-        textTheme: GoogleFonts.lexendTextTheme(),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
