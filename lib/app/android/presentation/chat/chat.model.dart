88cc29c31fd8fedcf3e8b4184212c4fe36890e90
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-29 03:21:07 +0000

    worked on getting the shrine_images

--- lib/app/android/presentation/chat/chat.model.dart
+++ lib/app/android/presentation/chat/chat.model.dart
@@ -5,13 +5,21 @@ class ChatModel {
   String lastMessage;
   DateTime timeStamp;
   int messageNumber;
+  int id;
 
   ChatModel({
     required this.avatar,
     required this.name,
     required this.lastMessage,
     required this.timeStamp,
-    required this.messageNumber
+    required this.messageNumber,
+    required this.id
   });
+
+  String get assetName => '$id-0.jpg';
+  String get assetPackage => 'shrine_images';
+
+  @override
+  String toString() => '$name (id=$id)';
 
 }
