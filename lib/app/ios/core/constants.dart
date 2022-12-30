ae350372435025860655cbca2481352c14783ba5
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-30 22:24:10 +0000

    created cupertino theme

---
+++ lib/app/ios/core/constants.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/cupertino.dart';
+
---
+++ lib/app/ios/core/theme.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
+
+class CupertinoTheme {
+
+  static CupertinoThemeData main({bool isDark = false}) {
+    return CupertinoThemeData();
+  }
+}
